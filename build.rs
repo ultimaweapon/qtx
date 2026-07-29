3787ffc9b0413afb8c4e3a8291a66928f91eb23e
Putta Khunchalee <ultimaweapon@outlook.com>
2026-07-29 22:03:18 +0700

    Fixes incorrect Qt libs on Linux

--- build.rs
+++ build.rs
@@ -17,33 +17,40 @@ fn main() {
     println!("cargo::rustc-link-search=native={}", out.to_str().unwrap());
     println!("cargo::rustc-link-lib=static=qtx");
 
-    // Get path for Qt libraries.
-    let qmake = Command::new("qtpaths6")
-        .arg("--query")
-        .arg("QT_INSTALL_LIBS")
-        .output()
-        .unwrap();
-
-    assert!(qmake.status.success());
-
     // Link Qt.
-    let libs = std::str::from_utf8(&qmake.stdout).unwrap();
     let target = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
-
-    match target.as_str() {
+    let add_path = match target.as_str() {
         "linux" => {
-            println!("cargo::rustc-link-search=native={libs}");
-            println!("cargo::rustc-link-lib=QtCore");
-            println!("cargo::rustc-link-lib=QtGui");
-            println!("cargo::rustc-link-lib=QtWidgets");
+            println!("cargo::rustc-link-lib=Qt6Core");
+            println!("cargo::rustc-link-lib=Qt6Gui");
+            println!("cargo::rustc-link-lib=Qt6Widgets");
+
+            false
         }
         "macos" => {
-            println!("cargo::rustc-link-search=framework={libs}");
             println!("cargo::rustc-link-lib=framework=QtCore");
             println!("cargo::rustc-link-lib=framework=QtGui");
             println!("cargo::rustc-link-lib=framework=QtWidgets");
             println!("cargo::rustc-link-lib=c++");
+
+            true
         }
         _ => todo!(),
+    };
+
+    if add_path {
+        // Get path for Qt libraries.
+        let qmake = Command::new("qtpaths6")
+            .arg("--query")
+            .arg("QT_INSTALL_LIBS")
+            .output()
+            .unwrap();
+
+        assert!(qmake.status.success());
+
+        // Add to search path.
+        let libs = std::str::from_utf8(&qmake.stdout).unwrap();
+
+        println!("cargo::rustc-link-search=framework={libs}");
     }
 }
